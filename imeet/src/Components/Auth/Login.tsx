9901e071a84c0ff880c9227b37737c99cebc127e
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-11-03 02:35:42 +0530

    feat: setup next-aut and complete logic of login

--- imeet/src/Components/Auth/Login.tsx
+++ imeet/src/Components/Auth/Login.tsx
@@ -1,13 +1,54 @@
-import React from "react";
+"use client";
+import React, { ChangeEvent, useState } from "react";
 import { Button, Input } from "@/Exports/UI";
+import { signIn } from "next-auth/react";
 
 const Login = () => {
+  const [meta, setMeta] = useState({
+    uid: "",
+    password: "",
+  });
+
+  const handleInputChange = (e: ChangeEvent) => {
+    const { name, value } = e.target;
+
+    setMeta((prevMeta) => ({ ...prevMeta, [name]: value }));
+  };
+
+  const handleSubmit = async (e: SubmitEvent): Promise<any> => {
+    e.preventDefault();
+
+    const response = await signIn("credentials", {
+      uid: meta.uid,
+      password: meta.password,
+      redirect: false,
+    });
+    console.log("repsonse", response);
+    // toast.
+  };
+
   return (
     <div className="flex-1 p-8 bg-white">
       <h2 className="text-2xl font-bold mb-6">Log In</h2>
-      <Input type="text" placeholder="UID"  className="mb-4" />
-      <Input type="text" placeholder="Password" className="mb-4" />
-      <Button className="w-full">Log In</Button>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          name="uid"
+          placeholder="UID"
+          className="mb-4"
+          onChange={handleInputChange}
+        />
+        <Input
+          type="text"
+          name="password"
+          placeholder="Password"
+          className="mb-4"
+          onChange={handleInputChange}
+        />
+        <Button className="w-full" type="submit">
+          Log In
+        </Button>
+      </form>
     </div>
   );
 };
